a71f36006d1b2ad76617aff3cc3f8a29f1d0d9d6
Elielson de Oliveira <elielsonoliveira1@outlook.com>
2022-03-13 19:25:28 -0300

    feat: Adicionada a biblioteca - linkedlist\linkedlist.dll

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using linkedlist;
 
 namespace search
 {
@@ -7,11 +8,23 @@ namespace search
     {
         static void Main(string[] args)
         {
+			LinkedList<string> arquivosEncontrados = null;	// Cria uma lista para os arquivos encontrados
+            LinkedList<string> listaDeArquivos = null;    	// Cria uma lista para todos os arquivos no diretorio e subdiretorios
 			
             Console.WriteLine("Search versão 1.0");
-			Console.WriteLine("Uso:\nsearch <opcao> <arquivo>");
+			Console.WriteLine("Uso:\nsearch <arquivo>");
 				
             Console.WriteLine();
+			
+			string diretorioAtual = Directory.GetCurrentDirectory();		// Obtem o diretorio atual
+			if (args.Length == 1)											// Verifica se existe arquivo a ser procurado
+			{
+				Console.WriteLine("Arquivo procurado: {0}", args[0]);
+				Console.WriteLine("Carregando arquivos...");
+				
+			}
+			else
+				Console.WriteLine("Nenhum arquivo a ser procurado.");
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,30 @@
+# Search
+
+O search é um programa de busca de arquivos para o Windows CMD (Prompt de comando). Ele 
+possibilita uma busca rápida de um arquivo e exibe os locais no computador que contenham 
+o arquivo procurado.
+
+## Pré-requisitos
+
+É necessario ter .NET core instalado.
+
+## Compilação
+
+Dentro da pasta do programa compile o programa executando o comando.
+
+> dotnet build
+
+## Instalação
+
+A instalação e feita criando um local para os arquivos manualmente, por exemplo uma pasta C:\Program Files\Search
+e em seguida copiando todos os arquivos de execução presentes na pasta \bin do projeto para a pasta criada.
+Para executar o programa, deve-se adicionar o local dos executáveis a variável de ambiente do Windows PATH.
+
+## Versão
+
+v1.0
+
+## Autor
+
+Elielson de Oliveira
+elielsonoliveira1@outlook.com
