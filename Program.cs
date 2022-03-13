2b3ae74cdf7d02d29da12a1f0e5b2467e6a03d45
Elielson de Oliveira <elielsonoliveira1@outlook.com>
2022-03-13 12:44:14 -0300

    feat: Commit do projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			
+            Console.WriteLine("Search versão 1.0");
+			Console.WriteLine("Uso:\nsearch <opcao> <arquivo>");
+				
+            Console.WriteLine();
+        }
+    }
+}
